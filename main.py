c14280df4452a0f422c47d89e86825b5fc919c78
AleksandrBara <alexbar130379@gmail.com>
2023-01-26 23:01:32 +0300

    Send msg to telegram.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import requests
 from dotenv import load_dotenv
 from pprint import pprint
 import  time
+from telegram import Bot
 
-def get_user_reviews(dvmn_token):
+DVMN_URL = 'https://dvmn.org/api/long_polling/'
+
+def get_user_reviews(dvmn_token, bot_token, chat_id):
     # url = 'https://dvmn.org/api/user_reviews/'
     timestamp = time.time()
-    url = 'https://dvmn.org/api/long_polling/'
+    url = DVMN_URL
     while True:
       try:
         response = requests.get(
@@ -18,7 +21,20 @@ def get_user_reviews(dvmn_token):
         )
         response.raise_for_status()
         reviews = response.json()
-        pprint(reviews)
+
+        status = reviews.get('status')
+        if status == 'found':
+          timestamp = reviews['last_attempt_timestamp']
+          result = reviews.get("new_attempts")[0]
+          message = 'Работа проверена'
+
+          bot = Bot(token=bot_token)
+          bot.send_message(
+            chat_id=chat_id,
+            text=message,
+            parse_mode="markdown")
+        elif status == 'timeout':
+          timestamp = reviews['timestamp_to_request']
 
       except requests.exceptions.HTTPError as err:
         print(f"Возникла ошибка при выполнении HTTP-запроса:\n{err}")
@@ -29,6 +45,9 @@ def get_user_reviews(dvmn_token):
 if __name__ == '__main__':
   load_dotenv()
   dvmn_token = os.environ["DVMN_TOKEN"]
-  get_user_reviews(dvmn_token)
+  bot_token = os.environ["BOT_TOKEN"]
+  chat_id = os.environ["CHAT_ID"]
+
+  get_user_reviews(dvmn_token, bot_token, chat_id)
 
 
