e89976464f543375c66c2b1be0428d2eac71610d
AleksandrBara <alexbar130379@gmail.com>
2023-01-29 22:14:56 +0300

    Send logs to Telegram to admin user.

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ import requests
 from dotenv import load_dotenv
 import time
 from telegram import Bot
+import logging
+from logging.handlers import RotatingFileHandler
 
 DVMN_URL = 'https://dvmn.org/api/long_polling/'
+
+logger = logging.getLogger()
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def get_user_reviews(dvmn_token, bot_token, chat_id):
@@ -40,16 +56,20 @@ def get_user_reviews(dvmn_token, bot_token, chat_id):
                 message = 'Преподаватель проверил работу:' \
                           ' [{}]({}) \n {}'.format(title, url, result)
                 bot = Bot(token=bot_token)
+
                 bot.send_message(
                     chat_id=chat_id,
                     text=message,
                     parse_mode='markdown')
+                logging.info('Бот отправил сообщение!')
+
             elif status == 'timeout':
                 timestamp = reviews['timestamp_to_request']
 
         except requests.exceptions.HTTPError as err:
-            print(f"Возникла ошибка при выполнении HTTP-запроса:\n{err}")
-        except requests.exceptions.ConnectionError:
+            logging.error(f"Возникла ошибка при выполнении HTTP-запроса:\n{err}")
+        except requests.exceptions.ConnectionError as err:
+            logging.error(f"Ошибка подключения:\n{err}")
             time.sleep(90)
             continue
         except requests.exceptions.ReadTimeout:
@@ -61,5 +81,11 @@ if __name__ == '__main__':
     dvmn_token = os.environ["DVMN_TOKEN"]
     bot_token = os.environ["BOT_TOKEN"]
     chat_id = os.environ["CHAT_ID"]
+
+    bot = Bot(token=bot_token)
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    logging.info('Бот стартовал!')
 
     get_user_reviews(dvmn_token, bot_token, chat_id)
